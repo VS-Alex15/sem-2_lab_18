02c8a99d22a94cb3e3f473b37451c45b5b07de65
vladimir <vladimir.shadymov@gmail.com>
2017-02-25 15:41:34 +0300

    Strongly-Connected-Components

--- E.py
+++ E.py
@@ -13,16 +13,13 @@ class LinkedList:
     def push_front(self, data):
         self._link = [data, self._link]
 
+    def empty(self):
+        return not(self._link)
+
     def pop(self,data):
         p = self._link
         if p == None:
             return None
-        #elif p[1]==None:
-         #   k = None
-          #  if p[0]==data:
-           #     k = data
-            #    p = p[1]
-            #return k
         else:
             k = None
             f = p
@@ -36,3 +33,60 @@ class LinkedList:
                 else:
                     self._link = p[1]
             return k
+    def front(self):
+        if self.empty():
+            return None
+        else:
+            return self._link[0]
+
+vertexes = LinkedList()
+
+def dfs_straight(vertex, graph, used = set()):
+    used.add(vertex)
+    for neighbour in graph[vertex]:
+        if neighbour not in used:
+            dfs_straight(neighbour, graph, used)
+    vertexes.push_front(vertex)
+
+
+def dfs_reverse(vertex, graph, used = set()):
+    used.add(vertex)
+    vertexes.pop(vertex)
+    linked_components[k].add(vertex)
+    for neighbour in graph[vertex]:
+        if neighbour not in used:
+            dfs_reverse(neighbour, graph, used)
+
+N = int(input())
+M = int(input())
+
+graph = [[] for i in range(N)]
+graph_full = [[] for i in range(N)]
+graph_T = [[] for i in range(N)]
+
+for edge in range(M):
+    a, b = [int(x) for x in input().split()]
+
+    graph[a].append(b)
+
+    graph_T[b].append(a)
+
+    graph_full[a].append(b)
+    graph_full[b].append(a)
+
+used = set()
+for vertex in range(N):
+    if vertex not in used:
+        dfs_straight(vertex, graph, used)
+
+linked_components = []
+
+k = -1
+used = set()
+
+while not vertexes.empty():
+    k += 1
+    linked_components.append(set())
+    vertex = vertexes.front()
+    dfs_reverse(vertex, graph_T, used)
+print(len(linked_components))
